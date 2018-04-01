ec89af0e255e691a2ee7283e7174aec462eb8155
Naor Kolet <naork94@gmail.com>
2018-04-01 21:43:04 +0300

    added print

--- src/Company/Importer.java
+++ src/Company/Importer.java
@@ -10,7 +10,10 @@ public class Importer {
     }
 
     public Computer importComputer(Order order) {
+        System.out.println("Importer forwards request to Supplier.");
         Supplier supplier = new Supplier();
-        return supplier.prepareComputer(order);
+        Computer pc =  supplier.prepareComputer(order);
+        System.out.println("Importer returns the computer to Yossi-Computer.");
+        return pc;
     }
 }
--- src/Company/Supplier.java
+++ src/Company/Supplier.java
@@ -9,12 +9,13 @@ public class Supplier {
     }
 
     public Computer prepareComputer(Order order){
+        System.out.println("Supplier prepares an unassembled computer.");
         String cpu = order.getCpu();
         String motherboard = order.getMotherboard();
         String peripherals = order.getPeripherals();
         String screen = order.getScreen();
-
         Computer computer = new Computer(cpu, motherboard, peripherals, screen);
+        System.out.println("Supplier returns the computer to Importer.");
         return computer;
     }
 }
