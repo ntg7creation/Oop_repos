ab8f121cbe09d93b90c5c1a25c886076e2df59da
natai <ntg7creation@gmail.com>
2018-06-21 18:47:18 +0300

    fixing

--- src/assiment4/entitys/Ghosts/Ghost_Red.java
+++ src/assiment4/entitys/Ghosts/Ghost_Red.java
@@ -1,13 +1,16 @@
 package assiment4.entitys.Ghosts;
 
 import assiment4.Storage.ImageLoader;
+import assiment4.entitys.Attacks.Fire_Ball;
 import assiment4.entitys.Pacmans.Pacman_Blue;
 import assiment4.entitys.Pacmans.Pacman_Red;
 import assiment4.entitys.Pacmans.Pacman_Yellow;
 
 public class Ghost_Red extends Ghost {
 
+
 	private final int id = 32;
+
 
 	public Ghost_Red() {
 		sprites = ImageLoader.getInstance().getRedGhost();
@@ -25,6 +28,7 @@ public class Ghost_Red extends Ghost {
 		} else {
 			dealyTime++;
 		}
+		
 	}
 
 	@Override
